311da5e67dc19f6763b969b2b45d635f1617812f
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2021-07-01 21:49:56 -0500

    Formulario y extras
    Cambios generales en el diseño e inclusion de un formulaio

---
+++ formulario.js
@@ -0,0 +1,7 @@
+function captura(){
+    var correo=document.getElementById("correo").value;
+    console.log(correo)
+
+    var resena=document.getElementById("resena").value;
+    console.log(resena)
+}

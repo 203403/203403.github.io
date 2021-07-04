c60192f8de55098cad6d7d6fd4a5b5b64b5a8b34
203413 <77992655+203413@users.noreply.github.com>
2021-07-03 22:45:17 -0500

    Añadido de los alerts

--- formulario.js
+++ formulario.js
@@ -1,7 +1,13 @@
 function captura(){
     var correo=document.getElementById("correo").value;
     console.log(correo)
+    if (correo==""){
+        alert("El correo es necesario")
+    }
 
     var resena=document.getElementById("resena").value;
     console.log(resena)
+    if (resena==""){
+        alert("Falta la reseña")
+    }
 }

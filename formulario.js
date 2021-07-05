1f7f1d23bcc40f77477af69c7fbeae13dc44fad4
Francisco Omar Franco Espinosa <77992667+203403@users.noreply.github.com>
2021-07-05 09:23:16 -0500

    Actualizacion
    Quite el ultimo cambio realizado (quite un alert de prueba)

--- formulario.js
+++ formulario.js
@@ -4,7 +4,6 @@ function captura(){
     if (correo==""){
         alert("El correo es necesario")
     }
-    alert("Para ver si funciona")
 
     var resena=document.getElementById("resena").value;
     console.log("Resena: " + resena)

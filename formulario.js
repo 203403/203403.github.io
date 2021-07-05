d5477da169913534b8e771df82bae93b656f8a7f
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2021-07-05 09:37:51 -0500

    Cambios al footer y al js
    Ajustes al footer en todas las paginas.
    Y ajustes al js.

--- formulario.js
+++ formulario.js
@@ -1,7 +1,17 @@
 function captura(){
     var correo=document.getElementById("correo").value;
-    console.log(correo)
+
+    if (correo==""){
+        alert("El correo es necesario")
+    }else{
+		console.log("Correo: " + correo)	
+	}
 
     var resena=document.getElementById("resena").value;
-    console.log(resena)
+    if (resena==""){
+        alert("Falta la reseña")
+    }else{
+		console.log("Resena: " + resena)
+	}
+    alert("Gracias por enviar su resena, la revisaremos y le enviaremos una respuesta")
 }
